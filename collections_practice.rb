9a864af315559da33469fa1f2edf6bfe9c41d95b
Jeff Benton <sikletz@gmail.com>
2019-01-13 01:43:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,14 +1,19 @@
 def sort_array_asc(arr)
-  arr.sort { |a,b| a <=> b}
+  arr.sort { |a, b| a <=> b}
 end
 
 def sort_array_desc(arr)
+  arr.sort { |a, b| b <=> a}
 end
 
 def sort_array_char_count(arr)
+  arr.sort { |a, b| a.length <=> b.length}
 end
 
 def swap_elements(arr)
+  temp = arr[1]
+  arr[1] = arr[2]
+  arr[2] = temp
 end
 
 def reverse_array(arr)
